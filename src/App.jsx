1871e4272b66d7deb3bd8b83b9700dee30370722
Yamen Sharaf <YamenSharaf@Gmail.com>
2018-04-06 18:30:24 +0200

    Initial project scaffold

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Button, Grid, Header, List, Segment } from 'semantic-ui-react'
+
+import 'styling/app.less'
+
+const App = () => (
+  <div className="app-container">
+    <h1>
+      Here we go
+    </h1>
+  </div>
+)
+
+export default App
